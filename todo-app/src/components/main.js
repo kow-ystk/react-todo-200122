9209b52086d4b247414862a07fe0051a4415321b
Koki Kamezono <kow1ag25@gmail.com>
2020-01-25 16:19:21 +0900

    [Add]テキストを取得し、リストへ追加する処理を追加。

--- todo-app/src/components/main.js
+++ todo-app/src/components/main.js
@@ -5,14 +5,22 @@ import {ToDoAppList} from './applist';
 export class Todo extends React.Component {
   constructor(props){
     super();
-    this.state = {tasks: props.tasksList};
+    this.state = {tasks: props.tasks};
+    this.updateList = this.updateList.bind(this);
+  }
+
+  // リストの更新
+  updateList(text) {
+    var updatedTasks = this.state.tasks;
+    updatedTasks.push(text);
+    this.setState({tasks: updatedTasks}); // stateを更新
   }
 
   render(){
     return(
       <div>
         <h1>Todo App</h1>
-        <AddNewTask />
+        <AddNewTask updateList={this.updateList} />
         <ToDoAppList tasks={this.state.tasks} />
       </div>
     );
