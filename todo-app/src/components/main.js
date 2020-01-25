97d10fcd58143c8b4d16f628f46a7b2e325a6c74
Koki Kamezono <kow1ag25@gmail.com>
2020-01-25 16:43:00 +0900

    [Add]localstorageを追加。

--- todo-app/src/components/main.js
+++ todo-app/src/components/main.js
@@ -7,19 +7,30 @@ export class Todo extends React.Component {
     super();
     this.state = {tasks: props.tasks};
     this.updateList = this.updateList.bind(this);
+    this.removeTask = this.removeTask.bind(this);
   }
 
   // リストの更新
   updateList(text) {
     var updatedTasks = this.state.tasks;
-    updatedTasks.push(text);
+    updatedTasks.unshift(text);
     this.setState({tasks: updatedTasks}); // stateを更新
+    // ローカルストレージ
+    this.updateLocalStorage(updatedTasks);
   }
 
+  // タスクの削除
   removeTask(text){
     var updatedTasks = this.state.tasks;
     updatedTasks.splice(updatedTasks.indexOf(text), 1);
     this.setState({tasks: updatedTasks});
+    // ローカルストレージ
+    this.updateLocalStorage(updatedTasks);
+  }
+
+  // ローカルストレージへデータを格納
+  updateLocalStorage(updatedTasks){
+    localStorage.setItem('storedTasks', JSON.stringify(updatedTasks));
   }
 
   render(){
--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -4,4 +4,12 @@ import {Todo} from './components/main';
 
 var tasksList = ["Task 1", "Task 2", "Task 3"];
 
+// ローカルストレージにtasksがある場合、そのデータを引っ張ってくる
+var tasks = localStorage.getItem('storedTasks');
+
+// tasksがある場合
+if(tasks){
+  tasksList = JSON.parse(tasks);
+}
+
 ReactDOM.render(<Todo tasks={tasksList} />, document.getElementById('todo'));
